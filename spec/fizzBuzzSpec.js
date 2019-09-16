41a6114650da44745dcddc6e84daa7543ef91374
Neil Campbell <neildcampbell@hotmail.com>
2019-09-16 16:35:31 +0100

    added second test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,9 +1,11 @@
 
 describe("FizzBuzz", function() {
-    // let num = []
-
     it("print numbers one to one hundred", function(){
         expect(num[1]).toBeGreaterThan(0);
         expect(num[99]).toBeLessThan(101);
     })
+
+   it("prints fizz if the number is divisble by 3", function(){
+     expect(num[2]).toEqual('fizz')
+   })
 })
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,11 +1,16 @@
 let num = []
 let fizzBuzz = function(){
     for ( let i = 1; i < 101; i++) {
-        num.push(i);
-        console.log(i)
+  if (i % 3 == 0){
+    num.push('fizz')
+  }
+  else if (i % 5 == 0) {
+     num.push('buzz')
+  }
+  else {
+     num.push(i)
+  }
     }
 return num
-
 }
  fizzBuzz()
-
