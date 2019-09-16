305fe2bce4271c6df8e377e20f3af717b9754d26
Canvasbag989 <Canvasbag989@icloud.com>
2019-09-16 15:58:07 +0100

    spec and src modifed and test passing

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,9 @@
+
+describe("FizzBuzz", function() {
+    // let num = []
+
+    it("print numbers one to one hundred", function(){
+        expect(num[1]).toBeGreaterThan(0);
+        expect(num[99]).toBeLessThan(101);
+    })
+})
---
+++ src/fizzBuzz.js
@@ -0,0 +1,11 @@
+let num = []
+let fizzBuzz = function(){
+    for ( let i = 1; i < 101; i++) {
+        num.push(i);
+        console.log(i)
+    }
+return num
+
+}
+ fizzBuzz()
+
